6e781922d06608e77e344e5c79c06169bd0a81df
mohammedkoya7356 <muhammadkoya04@gmail.com>
2026-02-12 13:05:20 +0530

    add full project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Projects/Navbar";
 import Projects from "./components/Projects/Projects";
 import "./index.css";
 
 
 function App() {
   return (
-    <>
+    <><Navbar/>
       <Projects/>
          <div className="min-h-screen flex items-center justify-center">
        <h1 className="text-4xl font-bold text-red-500">
