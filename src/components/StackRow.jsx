f8e6c804030e2516442517bb9ecd1caf01aec145
mohammedkoya7356 <muhammadkoya04@gmail.com>
2026-03-30 10:45:15 +0530

    add full project

--- src/components/StackRow.jsx
+++ src/components/StackRow.jsx
@@ -4,29 +4,22 @@ export default function StackRow() {
   const tech = [
     { name: "MongoDB", class: "mongo" },
     { name: "Express", class: "express" },
-    { name: "React", class: "react" },
     { name: "Node", class: "node" },
     { name: "JavaScript", class: "js" }
   ];
 
   return (
     <section className="stack-section">
-      <h2 className="stack-title">MERN STACK</h2>
+      <h2 className="stack-title">SKILLS</h2>
 
       <div className="stack-row">
         {tech.map((item, i) => (
           <div key={i} className="stack-card">
-
-            {item.class === "react" ? (
-              <div className="logo react">
-                <div className="react-ring"></div>
-              </div>
-            ) : (
-              <div className={`logo ${item.class}`}>
-                {item.class === "js" && <span>JS</span>}
-                {item.class === "express" && <span>EX</span>}
-              </div>
-            )}
+            
+            <div className={`logo ${item.class}`}>
+              {item.class === "js" && <span>JS</span>}
+              {item.class === "express" && <span>EX</span>}
+            </div>
 
             <p>{item.name}</p>
           </div>
